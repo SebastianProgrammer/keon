2330b105fcee96cd983727d64ee71327fd7ffaba
cave-jhonson <danilogomez280073@correo.itm.edu.co>
2022-09-29 21:12:37 -0500

    avance pagina keon

--- components/Header.js
+++ components/Header.js
@@ -1,6 +1,8 @@
 import Link from "next/link";
 import styles from "../styles/Header.module.css";
 import { useState } from "react";
+import menu_icon from '../public/img/menuicon.svg'
+import Image from "next/image";
 const Header = () => {
   const [displayMenu, setDisplayMenu] = useState(false)
   return (
@@ -12,12 +14,7 @@ const Header = () => {
           </Link>
         </div>
         <button className={styles.btn_menu} onClick={()=> setDisplayMenu((open)=>!open)}>
-                      <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-menu-2" width="38" height="38" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ffffff" fill="none" stroke-linecap="round" stroke-linejoin="round">
-              <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-              <line x1="4" y1="6" x2="20" y2="6" />
-              <line x1="4" y1="12" x2="20" y2="12" />
-              <line x1="4" y1="18" x2="20" y2="18" />
-            </svg>
+            <Image src={menu_icon} width={38} height={38}></Image>
         </button>
       </div>
       {displayMenu && 
